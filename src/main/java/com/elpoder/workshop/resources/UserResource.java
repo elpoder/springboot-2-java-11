c684909a5d35c18ea0000cb7f3be710f345417ac
alexandre sobrinho <linuxman.df@gmail.com>
2021-05-19 22:16:21 -0300

    config banco, perfil teste e jpa

---
+++ src/main/java/com/elpoder/workshop/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.elpoder.workshop.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.elpoder.workshop.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "nome teste", "email teste", "fone teste", "password teste");
+		return ResponseEntity.ok().body(u);
+		
+	}
+}
