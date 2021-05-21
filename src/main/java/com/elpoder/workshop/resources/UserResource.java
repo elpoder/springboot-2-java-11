e31ff70afe0172e555249e8921d07073994310a4
alexandre sobrinho <linuxman.df@gmail.com>
2021-05-20 21:01:51 -0300

    classes servico find all e find by id coim endpoint

--- src/main/java/com/elpoder/workshop/resources/UserResource.java
+++ src/main/java/com/elpoder/workshop/resources/UserResource.java
@@ -1,20 +1,35 @@
 package com.elpoder.workshop.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.elpoder.workshop.entities.User;
+import com.elpoder.workshop.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "nome teste", "email teste", "fone teste", "password teste");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.FindAll();
 		
+		return ResponseEntity.ok().body(list);
+		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
